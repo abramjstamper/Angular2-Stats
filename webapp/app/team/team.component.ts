56a1c99daf56e6803a3113e69fc7467ad9904186
Abram Stamper <abram.j.stamper@me.com>
2017-01-02 11:44:53 -0500

    added team page

--- webapp/app/team/team.component.ts
+++ webapp/app/team/team.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Team } from '../shared/team';
+import { RosterService } from '../roster/roster.service';
 
 @Component({
   moduleId: module.id,
   selector: 'team',
-  templateUrl: 'team.html'
+  templateUrl: 'team.html',
+  providers: [
+    RosterService
+  ]
 })
 export class TeamComponent {
+
+  currentTeam: Team;
+
+      constructor(
+        private rosterService: RosterService
+      ){}
+
+      ngOnInit() {
+        this.currentTeam = this.rosterService.getTeam();
+      }
 
 }
